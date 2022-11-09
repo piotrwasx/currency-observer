616477368d5ed95957242eda6ecabe589f68ea9c
piotrwas <ns.piotr@gmail.com>
2022-11-09 11:55:50 +0100

    Initial Commit

---
+++ currency-observer/currency_observerApp.swift
@@ -0,0 +1,17 @@
+//
+//  currency_observerApp.swift
+//  currency-observer
+//
+//  Created by Piotr Waś on 09/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct currency_observerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ currency-observerUITests/currency_observerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  currency_observerUITestsLaunchTests.swift
+//  currency-observerUITests
+//
+//  Created by Piotr Waś on 09/11/2022.
+//
+
+import XCTest
+
+final class currency_observerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
